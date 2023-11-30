83896d21078e465c29532ff68cafa783bcbae09f
Wu Jian Gang <wujiangang@espressif.com>
2023-11-30 15:19:24 +0800

    feat: Add simple ov2640 component template

---
+++ drivers/cameras/ov2640/ov2640.c
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_camera.h"
+
+static const char *TAG = "ov2640";
+
+esp_camera_device_t ov2640_dvp_detect(const esp_camera_dvp_config_t *config)
+{
+    ESP_LOGI(TAG, "ov2640_dvp_detect");
+    return NULL;
+}
+
+#if CONFIG_CAMERA_OV2640_AUTO_DETECT
+ESP_CAMERA_DETECT_FN(ov2640_dvp_detect, CAMERA_INF_DVP)
+{
+    return ov2640_dvp_detect(config);
+}
+#endif
