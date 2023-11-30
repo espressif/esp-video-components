5a5c82d1cd2374888efee463d293a84cbc49b63c
Wu Jian Gang <wujiangang@espressif.com>
2023-11-30 15:20:30 +0800

    feat: Update ov5640

--- drivers/cameras/ov5640/ov5640.c
+++ drivers/cameras/ov5640/ov5640.c
@@ -8,8 +8,15 @@
 
 static const char *TAG = "ov5640";
 
-esp_camera_device_t ov5640_detect(void)
+esp_camera_device_t ov5640_csi_detect(const esp_camera_csi_config_t *config)
 {
-    ESP_LOGI(TAG, "ov5640_detect");
+    ESP_LOGI(TAG, "ov5640_csi_detect");
     return NULL;
 }
+
+#if CONFIG_CAMERA_OV5640_AUTO_DETECT
+ESP_CAMERA_DETECT_FN(ov5640_csi_detect, CAMERA_INF_CSI)
+{
+    return ov5640_csi_detect(config);
+}
+#endif
