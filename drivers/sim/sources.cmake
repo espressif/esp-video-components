136c5d55d66bc8840564b00e01b3b4873af060b1
Wu Jian Gang <wujiangang@espressif.com>
2023-12-05 20:45:51 +0800

    feat: Change folder structure of sim

--- drivers/sim/sources.cmake
+++ drivers/sim/sources.cmake
@@ -1,4 +1,6 @@
 if(CONFIG_SIMULATED_INTF)
     list(APPEND srcs "drivers/sim/sim_init.c"
                      "drivers/sim/sim_video.c")
+
+    list(APPEND include_dirs "drivers/sim/include")
 endif()
