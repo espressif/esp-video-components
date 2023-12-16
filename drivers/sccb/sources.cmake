347db587a4fd22be900c27aa1b7537f1654d8a51
Wu Jian Gang <wujiangang@espressif.com>
2023-12-16 19:46:23 +0800

    feat: Enable SCCB driver for other chips

--- drivers/sccb/sources.cmake
+++ drivers/sccb/sources.cmake
@@ -1,6 +1,2 @@
-if(CONFIG_IDF_TARGET_ESP32P4)
-    # Current I2C and I3C SCCB driver are only for P4.
-
-    list(APPEND srcs "drivers/sccb/sccb_i2c.c")
-    list(APPEND include_dirs "drivers/sccb/include")
-endif()
+list(APPEND srcs "drivers/sccb/sccb_i2c.c")
+list(APPEND include_dirs "drivers/sccb/include")
