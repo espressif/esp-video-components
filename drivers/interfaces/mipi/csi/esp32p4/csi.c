1301abeb98fc72844357f2e10c2463002f0bf1d2
Wang Yu Xin <wangyuxin@espressif.com>
2023-12-29 19:29:16 +0800

    fix: add get alloc_size API

--- drivers/interfaces/mipi/csi/esp32p4/csi.c
+++ drivers/interfaces/mipi/csi/esp32p4/csi.c
@@ -474,3 +474,11 @@ esp_err_t esp_mipi_csi_new_buffer_available(esp_mipi_csi_handle_t handle)
 
     return ESP_OK;
 }
+
+uint32_t esp_mipi_csi_get_fb_size(esp_mipi_csi_handle_t handle)
+{
+    MIPI_CSI_NULL_POINTER_CHECK(MIPI_CSI_TAG, handle);
+    esp_mipi_csi_obj_t *csi_cam_obj = (esp_mipi_csi_obj_t *)handle;
+
+    return csi_cam_obj->driver_config->fb_size;
+}
