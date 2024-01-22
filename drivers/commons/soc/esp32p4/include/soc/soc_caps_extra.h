d5ee3e9d48212d148b664f2b192816a158414a7b
Wang Yu Xin <wangyuxin@espressif.com>
2024-01-22 11:39:08 +0800

    fix: move csi_support to fix redefined warning

--- drivers/commons/soc/esp32p4/include/soc/soc_caps_extra.h
+++ drivers/commons/soc/esp32p4/include/soc/soc_caps_extra.h
@@ -11,6 +11,3 @@
 /*-------------------------- CAM CAPS ----------------------------------------*/
 #define SOC_CAM_SUPPORTED                    (1)  /*!< CAM is supported */
 #define SOC_CAM_SUPPORT_RGB_YUV_CONV         (1)  /*!< Support color format conversion between RGB and YUV */
-
-/*-------------------------- MIPI CSI CAPS ----------------------------------------*/
-#define SOC_MIPI_CSI_SUPPORTED               (1)  /*!< MIPI_CSI is supported */
