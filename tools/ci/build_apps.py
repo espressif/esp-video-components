4aaa816ea5c5ca3fb44315803dd02ef449016c42
Xiao Xufeng <xiaoxufeng@espressif.com>
2024-03-25 17:51:36 +0800

    ci: make build test simpler

--- tools/ci/build_apps.py
+++ tools/ci/build_apps.py
@@ -1,34 +1,42 @@
 # SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 
+import subprocess, sys
 from pathlib import Path
-from idf_build_apps import find_apps, build_apps, setup_logging
 from idf_build_apps.constants import SUPPORTED_TARGETS
 
-
 if __name__ == '__main__':
-    extra_default_build_targets = ['esp32p4']
-    app_path = '.'
+    preview_targets = ['esp32p4']
+    root = '.'
 
-    setup_logging(verbose=True, colored=True)
+    args = [
+        'build',
+        # Find args
+        '-p',
+        root,
+        '-t',
+        'all',
+        '--build-dir',
+        'build_@t_@w',
+        '--build-log',
+        'build_log.txt',
+        '--size-file',
+        'size.json',
+        '--recursive',
+        '--check-warnings',
+        # Build args
+        '--collect-size-info',
+        'size_info.txt',
+        '--keep-going',
+        '--copy-sdkconfig',
+        '-v',
+    ]
 
-    apps = find_apps(
-        app_path,
-        recursive=True,
-        target='all',
-        build_dir='build_@t_@w',
-        build_log_path='build_log.txt',
-        size_json_path='size.json',
-        check_warnings=True,
-        manifest_files=[str(p) for p in Path(app_path).glob('**/.build-test-rules.yml')],
-        default_build_targets=SUPPORTED_TARGETS + extra_default_build_targets,
-        manifest_rootpath=app_path,
-    )
+    args += ['--default-build-targets'] + SUPPORTED_TARGETS + preview_targets
 
-    build_apps(
-        sorted(apps),
-        dry_run=False,
-        keep_going=True,
-        collect_size_info='size_info.txt',
-        copy_sdkconfig=True,
-    )
+    manifests = [str(p) for p in Path(root).glob('**/.build-test-rules.yml')]
+    if manifests:
+        args += ['--manifest-file'] + manifests + ['--manifest-rootpath', root]
+
+    ret = subprocess.run(['idf-build-apps', *args])
+    sys.exit(ret.returncode)
