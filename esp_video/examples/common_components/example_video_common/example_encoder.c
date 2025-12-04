02354b459c50dbae6fdb227a09396837f7d160aa
Wu Jian Gang <wujiangang@espressif.com>
2025-12-04 16:05:02 +0800

    feat(esp_video): Update to use new byte order

--- esp_video/examples/common_components/example_video_common/example_encoder.c
+++ esp_video/examples/common_components/example_video_common/example_encoder.c
@@ -123,8 +123,23 @@ esp_err_t example_encoder_init(example_encoder_config_t *config, example_encoder
         jpeg_enc_config.subsampling = JPEG_SUBSAMPLE_GRAY;
         jpeg_enc_input_src_size = config->width * config->height;
         break;
-    case V4L2_PIX_FMT_YUV422P:
+    case V4L2_PIX_FMT_UYVY:
+        jpeg_enc_config.src_type = JPEG_PIXEL_FORMAT_CbYCrY;
+        jpeg_enc_config.subsampling = JPEG_SUBSAMPLE_422;
+        jpeg_enc_input_src_size = config->width * config->height * 2;
+        break;
+    case V4L2_PIX_FMT_YUYV:
         jpeg_enc_config.src_type = JPEG_PIXEL_FORMAT_YCbYCr;
+        jpeg_enc_config.subsampling = JPEG_SUBSAMPLE_422;
+        jpeg_enc_input_src_size = config->width * config->height * 2;
+        break;
+    case V4L2_PIX_FMT_RGB565:
+        jpeg_enc_config.src_type = JPEG_PIXEL_FORMAT_RGB565_LE;
+        jpeg_enc_config.subsampling = JPEG_SUBSAMPLE_422;
+        jpeg_enc_input_src_size = config->width * config->height * 2;
+        break;
+    case V4L2_PIX_FMT_RGB565X:
+        jpeg_enc_config.src_type = JPEG_PIXEL_FORMAT_RGB565_BE;
         jpeg_enc_config.subsampling = JPEG_SUBSAMPLE_422;
         jpeg_enc_input_src_size = config->width * config->height * 2;
         break;
